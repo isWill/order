d8e9fb626fde37a970546c150acf49552c827f00
吴宝华 <baohua.wu@zerotech.com>
2017-02-22 11:32:17 +0800

    add

---
+++ src/components/star/star.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="star" :class="starType">
+    <span v-for="itemClass in itemClasses" :class="itemClass" class="star-item"></span>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      size: {
+        type: Number
+      },
+      score: {
+        type: Number
+      }
+    },
+    computed: {
+      starType() {
+        return 'star-' + this.size;
+      }
+    }
+  };
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .star
+    font-size: 0
+    .star-item
+      display: inline-block
+      background-repeat: no-repeat
+    &.star-48
+      .star-item
+        width: 20px
+        height: 20px
+        margin-right: 22px
+        background-size: 20px 20px
+        &.on
+          bg-image()
+    &.star-36
+    &.star-24
+</style>
