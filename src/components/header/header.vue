3669c318a49b9589140bec3d45d813995772caf9
吴宝华 <baohua.wu@zerotech.com>
2017-02-24 17:51:51 +0800

    左侧menu布局

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -141,7 +141,7 @@
             width: 12px
             height: 12px
             margin-right: 4px
-            background-size: 12px
+            background-size: 12px 12px
             background-repeat: no-repeat
             &.decrease
               bg-image('decrease_1')
