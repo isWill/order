f2d9b46c3125f444e4a45c5d0abeaa9444cf52eb
Wu Baohua <wilhelm2014@163.com>
2017-03-20 22:15:35 +0800

    部分代码升级到2.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,8 @@ module.exports = {
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'semi':['error','always'],
+    'semi': ['error', 'always'],
+    'no-tabs': 0,
     'indent': 0,
     'space-before-function-paren': 0
   }
